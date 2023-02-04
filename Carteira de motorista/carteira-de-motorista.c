a738990e2ead42ca4de52caa897e5d818581b4e8
Filipe Tietbohl <filipedebarros@hotmail.com>
2023-02-03 21:00:26 -0300

    Utilizando IF

---
+++ Carteira de motorista/carteira-de-motorista.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+    int idade;
+    printf("Você tem quantos anos?\n");
+    scanf("%d", &idade);
+
+    if (idade > 18 || idade == 18)
+    {
+        printf("\nVocê tem idade suficiente para tirar a carteira de motorista");
+    }
+    else
+    {
+        printf("\nVocê ainda nÃ£o tem idade sufieicnete para tirar a carteira de motorista.");
+    }
+}
